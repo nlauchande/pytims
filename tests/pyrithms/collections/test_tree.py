8aff4c6751574aa3fb70ec53e28534c0c964430e
nlauchande <nlauchande@gmail.com>
2020-02-15 20:14:39 +0200

    Unit test placeholder and main file

--- tests/pyrithms/collections/test_tree.py
+++ tests/pyrithms/collections/test_tree.py
@@ -4,22 +4,16 @@ alternatives to Python's general purpose built-in containerslist
 * LinkedList   factory function for creating tuple subclasses with named fields
 
 """
-from pyrithms.collections.linked_list import LinkedList
+from pyrithms.collections.tree import BSTTree
 import pytest
 import unittest
 
 
-class LinkedListTest(unittest.TestCase):
-    def test_insertion(self):
-        linked_list = LinkedList()
+class TreeTest(unittest.TestCase):
 
-        linked_list.insert("10")
-        linked_list.insert("30")
-        linked_list.insert("15")
-        linked_list.insert("40")
-        linked_list.insert("50")
-        result = []
-        for i in iter(linked_list):
-            result.append(i.data)
-
-        assert result == ["50", "40", "15", "30", "10"]
+    tree = BSTTree()
+    tree.insert_element("10")
+    tree.insert_element("30")
+    tree.insert_element("15")
+    tree.insert_element("40")
+    tree.insert_element("50")
